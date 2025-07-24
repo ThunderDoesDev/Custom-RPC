f4be8fa06e43ebab9057fc0c7f5cd636faf8c6a3
Thunder <79953877+ThunderDoesDev@users.noreply.github.com>
2025-07-24 10:01:47 +1000

    Delete tests directory

--- tests/test.js
+++
@@ -1,8 +0,0 @@
-const { spawnSync } = require('child_process');
-const path = require('path');
-
-const result = spawnSync(process.execPath, [path.join(__dirname, 'runSetActivities.js')], {stdio: 'inherit'});
-if (result.status !== 0) {
-  throw new Error('setActivities failed');
-}
-console.log('All tests passed');
